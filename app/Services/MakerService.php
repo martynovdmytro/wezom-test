3503872510afd2ac1b406b73b40beaf3faf91616
Dmytro Martynov <dmytro_martynov@icloud.com>
2023-03-06 15:32:28 +0200

    clean and refactor

--- app/Services/MakerService.php
+++ app/Services/MakerService.php
@@ -8,13 +8,14 @@ class MakerService
 {
     public function index($request) {
         $input = $request->input('autocomplete');
+
         $maker = null;
 
         if (!empty($input)) {
             $maker = DB::select(
                 "select makers.name AS maker_name, brands.name AS brand_name from `makers` 
                         left join `brands` on makers.id=brands.maker_id
-                        where makers.name LIKE '%$input%'"
+                        where makers.name LIKE ?", ["%{$input}%"]
             );
         }
 
